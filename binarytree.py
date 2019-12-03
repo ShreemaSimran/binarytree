57c3e9fcb39644b3bc71368e5271b707807d3dda
ShreemaSimran <52849951+ShreemaSimran@users.noreply.github.com>
2019-12-03 16:34:58 +0530

    Update binarytree.py

--- binarytree.py
+++ binarytree.py
@@ -13,34 +13,44 @@ class Node:
     a.append(root.val)
   return a
 
-    def search(a,n):
-      low=0
-      high=num
-      while(low>0 and high<num):
-        if(a[num//2]==n):
-          print('found at',num//2)
-        else:
-          if(a[num//2]>n):
-            high=num//2
-          else:
-            low=num//2
-      return 0;
+    def search(a,n,low,high):
+      if(high>=low):
+        mid=low+high/2
+        count=0
+        if(a[num]==n):
+          print('Position is',num)
+        elif(a[mid]>n):
+          for i in range(0,mid-1):
+            if i == n:
+              count++
+              print('position is',count)
+        elif(a[mid]<n):
+          count=mid
+          for i in range(mid,high):
+            if i == n:
+              count++
+              print('position is',count)
         
+      
 
- root = Node(1)
- root.left = Node(2)
- root.right = Node(3)
- root.left.left = Node(4)
- root.left.right = Node(5)
- root.right.right = Node(7)
- root.right.left = Node(6)
- root.left.left.left = Node(8)
- root.left.left.right = Node(9)
- root.left.left.left.left = Node(10)
- root.left.left.left.right = Node(11)
-  num = 10
+ root = Node(20)
+ root.left = Node(15)
+ root.right = Node(25)
+ root.left.left = Node(10)
+ root.left.right = Node(18)
+ root.right.left = Node(22)
+ root.right.right = Node(28)
+ root.left.left.left = Node(5)
+ root.left.left.right = Node(11)
+ root.left.right.left = Node(16)
+ root.left.right.right = Node(19)
+ root.right.left.left = Node(21)
+ root.right.left.right = Node(23)
+ root.right.right.left = Node(21)
+ root.right.right.right = Node(23)
+ num = 15
  print('Enter the node to be traversed')
  n = int(input())
  a = postorder(root)
- search(a, n)
+ search(a, n,0,num-1)
  
